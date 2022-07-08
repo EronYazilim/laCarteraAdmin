78d4b01ef525fbdf6ccafcefb745faa1b12bde27
Berkan Çalasın <berkan.calasin@eronyazilim.com>
2022-07-08 14:57:13 +0300

    satis sayfasinin filtreleme islemi duzenlendi

--- admin/src/app/pages/kayitlar/satisKayitlari/satisKayitlari.ts
+++ admin/src/app/pages/kayitlar/satisKayitlari/satisKayitlari.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { formatDate } from "@angular/common";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { NgbModal, NgbModalConfig } from "@ng-bootstrap/ng-bootstrap";
 import { ToastrService } from "ngx-toastr";
@@ -26,12 +27,17 @@ export class satisKayitlariComponent implements OnInit {
         modalConfig.size = 'sm'
     }
 
+    @ViewChild('modalBayi') modalBayi: ElementRef
+
+    modalHeader = { title: '' }
+
     filterData = {
         ARAMA       : '',
         SS          : 1,
         KS          : 15,
         e_durum     : '',
         e_bayi_id   : '',
+        e_bayi_adi  : '',
         TARIH_BAS   : '',
         TARIH_SON   : '',
         ESKI_ID     : ''
@@ -59,6 +65,12 @@ export class satisKayitlariComponent implements OnInit {
         this.satisListele()
     }
     
+    
+    modalAc(content, size) {
+        this.modalConfig.size = size
+        this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', centered: true })
+    }
+
     async satisListele() {
         this.mainLoader = true
         this.satisListesi = await this.islem.WebServisSorguSonucu("GET", "satisIslemleri/satisListesi", this.filterData)
@@ -72,5 +84,16 @@ export class satisKayitlariComponent implements OnInit {
         this.bayiKayitlari = await this.islem.WebServisSorguSonucu("GET", "bayiIslemleri/bayiListesi", this.bayiFilterData)
         if (Object.keys(this.bayiKayitlari).length == 0) { this.bayiKayitlari = null }
         this.bayiLoader = false
+    }
+
+    bayiFiltreleButton() {
+        this.modalHeader.title = 'Bayi Listesi'
+        this.modalAc(this.modalBayi, 'lg')
+    }
+
+    bayiSec(secilenBayi) {
+        this.filterData.e_bayi_id = secilenBayi.e_id
+        this.filterData.e_bayi_adi = secilenBayi.e_bayi_adi
+        this.modalService.dismissAll()
     }
 }
